035811133b8e14aa89411af04f6fe0bd75263d32
hrishikeshknair@gmail.com <hrishikeshknair@gmail.com>
2025-01-17 13:15:06 +0100

    Mapping_Drone_Random_exploration

--- Mapping_initialization_2D.py
+++ Mapping_initialization_2D.py
@@ -1,23 +1,48 @@
 import numpy as np
 
-grid_size = 100
-resolution = 0.1
-occupancy_grid = np.zeros((grid_size, grid_size), dtype=np.int8)
+def initialize_map( grid_size, resolution):
+
+    grid_size = 100
+    resolution = 0.1
+    occupancy_grid = np.zeros((grid_size, grid_size), dtype=0.5)
+    return occupancy_grid
 
 
-def update_grid( grid, drone_pos, ranges, angles, resolution):
-    for r , theta in zip(range, angles):
-        #convert to local coordinates
-        x_local = r * np.cos(theta)
-        y_local = r * np.sin(theta)
+def update_map( occupancy_grid, lidar_data, current_position, current_yaw):
+
+    x, y = current_position
+    rows, cols = occupancy_grid.shape
+    
+    for distance in lidar_data:
+        if distance > 0:
+            end_x = x+ distance *np.cos(np.radians(current_yaw))
+            end_y = y + distance * np.sin(np.radians(current_yaw))
+
+            #convert to grid indices
+            grid_x = int(end_x//(cols/rows))
+            grid_y = int(end_y//(rows/cols))
 
 
-        #convert to map coordinates
+            #update the occupancy grid ( mark as completed)
+            if 0<= grid_x < rows and 0<= grid_y <cols:
+                occupancy_grid[grid_x, grid_y] = 1.0 #occupancy space
 
-        x_map = int((drone_pos[0] + x_local) / resolution)
-        y_map = int((drone_pos[1] + y_local) / resolution)
 
 
-        #mark cell as occupied
+    return occupancy_grid
 
-        grid[x_map, y_map] = 1 
+    # for r , theta in zip(range, angles):
+    #     #convert to local coordinates
+    #     x_local = r * np.cos(theta)
+    #     y_local = r * np.sin(theta)
+
+
+    #     #convert to map coordinates
+
+    #     x_map = int((drone_pos[0] + x_local) / resolution)
+    #     y_map = int((drone_pos[1] + y_local) / resolution)
+
+
+    #     #mark cell as occupied
+
+    #     grid[x_map, y_map] = 1 
--- drone_control.py
+++ drone_control.py
@@ -1,6 +1,62 @@
 import asyncio
 from mavsdk import System
 from mavsdk.offboard import PositionNedYaw
+import random
+from Mapping_initialization_2D import initialize_map, update_map
+from Get_sensor_data import get_lidar_data
+import matplotlib.pyplot as plt
+
+async def random_exploration(drone, map_size, resolution, step_size):
+    # Initialize the map
+    occupancy_grid = initialize_map(grid_size = map_size, resolution = resolution)
+
+    #Define the movement boundaries
+    max_x, max_y = map_size[0] * resolution , map_size[1] * resolution
+
+    current_x, current_y, current_yaw = 0.0, 0.0, 0.0
+    explored_position = [(current_x, current_y)]
+
+    for _ in range(50): # limit the number of random movements
+
+        for yaw in range(0, 360, 2):
+            current_yaw = yaw
+            position_yaw = PositionNedYaw(current_x, current_y, -0.5, yaw)
+
+            await drone.offbaord.set_position_ned(position_yaw)
+            lidar_data = await get_lidar_data(drone)
+            occupancy_grid = update_map(occupancy_grid, lidar_data, (current_x, current_y), yaw)  
+            await asyncio.sleep(0.1)
+
+        plt.imshow(occupancy_grid, cmap= "grey")
+        plt.pause(0.1)
+
+        next_x = random.uniform(0, max_x)
+
+        next_y = random.uniform(0, max_y)
+
+        #Avoid revisitting the location
+
+        if (next_x, next_y) in explored_position:
+            continue
+        explored_position.append((next_x,next_y))
+
+        #Move to the new waypoint
+        print(f"Moving to new waypoint: ({next_x:.2f},{next_y:.2f})")
+        position_ned_yaw = PositionNedYaw(next_x, next_y, -0.5,current_yaw)
+        await drone.offbaord.set_position_ned(position_ned_yaw)
+        await asyncio.sleep(0.1)
+
+    return occupancy_grid
+
+
+
+
+
+
+
+
+
+
 
 async def run():
     # Connect to the PX4 SITL (default UDP port 14540)
@@ -23,24 +79,31 @@ async def run():
     await drone.action.takeoff()
     await asyncio.sleep(5)  # Hover for 5 seconds
 
-    # Set an initial setpoint
-    print("Setting initial position setpoint...")
-    position_ned_yaw = PositionNedYaw(north_m=0.0, east_m=0.0, down_m=-5.0, yaw_deg=0.0)
-    await drone.offboard.set_position_ned(position_ned_yaw)
 
-    # Start offboard mode
-    print("Starting offboard mode...")
-    try:
-        await drone.offboard.start()
-    except Exception as e:
-        print(f"Failed to start offboard mode: {e}")
-        return
+    #explore random
+    print(" starting random exploration")
+    final_map = await random_exploration(drone, map_size=(100,100), resolution= 0.1, step_size= 1.0)
 
-    # Move to a position (local NED frame)
-    print("Moving to position...")
-    position_ned_yaw = PositionNedYaw(north_m=0.0, east_m=5.0, down_m=-5.0, yaw_deg=90.0)
-    await drone.offboard.set_position_ned(position_ned_yaw)
-    await asyncio.sleep(5)
+
+
+    # # Set an initial setpoint
+    # print("Setting initial position setpoint...")
+    # position_ned_yaw = PositionNedYaw(north_m=0.0, east_m=0.0, down_m=-5.0, yaw_deg=0.0)
+    # await drone.offboard.set_position_ned(position_ned_yaw)
+
+    # # Start offboard mode
+    # print("Starting offboard mode...")
+    # try:
+    #     await drone.offboard.start()
+    # except Exception as e:
+    #     print(f"Failed to start offboard mode: {e}")
+    #     return
+
+    # # Move to a position (local NED frame)
+    # print("Moving to position...")
+    # position_ned_yaw = PositionNedYaw(north_m=0.0, east_m=5.0, down_m=-5.0, yaw_deg=90.0)
+    # await drone.offboard.set_position_ned(position_ned_yaw)
+    # await asyncio.sleep(5)
 
     # Land the drone
     print("Landing...")
